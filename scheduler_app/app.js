06e66b3eabc60be0bcd4dc340fd96015833c37fd
mdarkness1988 <42422496+mdarkness1988@users.noreply.github.com>
2018-08-18 02:52:51 +0100

    Add files via upload

--- scheduler_app/app.js
+++ scheduler_app/app.js
@@ -23,6 +23,46 @@ function checkForUpdates()
 {
 	setTimeout(function()
 	{
+
+
+var name = process.env.RUST_SERVER_NAME;
+var today = new Date(); var dd = today.getDate(); 
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear(); 
+if(dd<10) { dd = '0'+dd } if(mm<10) { mm = '0'+mm } today = dd + '/' + mm; 
+document.write(today);
+
+var servername = name + ' ' + today;
+
+
+var serverHostname = 'localhost';
+	var serverPort = process.env.RUST_RCON_PORT;
+	var serverPassword = process.env.RUST_RCON_PASSWORD;
+
+	var WebSocket = require('ws');
+	var ws = new WebSocket("ws://" + serverHostname + ":" + serverPort + "/" + serverPassword);
+	ws.on('open', function open()
+	{
+
+ws.send(createPacket("server.hostname \"" + servername + "\""));
+
+ws.close();
+
+}
+
+function createPacket(command)
+{
+	var packet =
+	{
+		Identifier: -1,
+		Message: command,
+		Name: "WebRcon"
+	};
+	return JSON.stringify(packet);
+}
+
+
+
 		if (debug) console.log("Running bash /update_check.sh");
 		child_process.exec('bash /update_check.sh', { /*timeout: 60 * 1000,*/ env: process.env }, function (err, stdout, stderr)
 		{
