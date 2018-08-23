ecda116a669a6ec987ee3b117c3552658998c271
mdarkness1988 <42422496+mdarkness1988@users.noreply.github.com>
2018-08-24 00:42:13 +0100

    Add files via upload

--- restart_app/app.js
+++ restart_app/app.js
@@ -72,11 +72,11 @@ function restart()
 		return;
 	}
 	isRestarting = true;
-   process.env.RESTARTING = 'true';
+   process.env.WIPE = 'true';
 
 	var serverHostname = 'localhost';
-	var serverPort = process.env.RUST_RCON_PORT;
-	var serverPassword = process.env.RUST_RCON_PASSWORD;
+	var serverPort = process.env.PORTFORWARD_RCON;
+	var serverPassword = process.env.RCONPW;
 
 	var WebSocket = require('ws');
 	var ws = new WebSocket("ws://" + serverHostname + ":" + serverPort + "/" + serverPassword);
