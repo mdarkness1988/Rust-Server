3c214eb252c6c296eafdb2b23895aa57e12d13f6
mdarkness1988 <42422496+mdarkness1988@users.noreply.github.com>
2018-08-30 12:35:43 +0100

    Add files via upload

--- apps/title_app/app.js
+++ apps/title_app/app.js
@@ -1,1 +1,46 @@
+#!/usr/bin/env node
+
+
+
+var request = require('request');
+
+	var serverHostname = 'localhost';
+	var serverPort = process.env.PORTFORWARD_RCON;
+	var serverPassword = process.env.PASSWORD;
+   var servername = process.env.NAME;
+   var wipedate = process.env.WIPED_TITLE;
+
+  var WebSocket = require('ws');
+	
+
+
+
+setTimeout(function()
+{
+   var ws = new WebSocket("ws://" + serverHostname + ":" + serverPort + "/" + serverPassword);
+	ws.on('open', function open()
+	{
+		setTimeout(function()
+		{
+			ws.send(createPacket(wipedate));
+        setTimeout(function()
+			 {
+				 ws.close(1000);
+	      }, 1000 * 20);
+		}, 1000);
+	});
+}, 1000 * 60 * 5);
+
+
+
+ function createPacket(command)
+ {
+	 var packet =
+	{
+		Identifier: -1,
+	  Message: command,
+    Name: "WebRcon"
+	 };
+	 return JSON.stringify(packet);
+ }
 
